a770fd775303d951bae552cdf5742635c5b32578
Jonathan Chiu <jonathanchiu7@gmail.com>
2014-11-13 21:05:35 -0500

    abstracted/cleaned up JS code, added style.css

--- main.js
+++ main.js
@@ -1,220 +1,256 @@
 $(function() {
 
-	$("#post-type").change(function() {
-		var post_type = $("#post-type").val();
-		if (post_type == "likes") {
-			$("#likes-radio-group").show();
-		} else {
-			$("#likes-radio-group").hide();
-		}
-	});
+  var api_key = "";
+  var user;
 
-	// Pagination - http://botmonster.com/jquery-bootpag/#.VERtcPnF9ie
+  initSearchBar();
+  handleLikesSelect();
 
-	var api_key = "api_key_goes_here";
-	var result = '';
-	var user = '';
-	var page_count;
+  /**
+   * Handles showing/hiding of additional filter options when
+   * user selects the "Likes" post type
+   */
+  function handleLikesSelect() {
+    $("#post-type").change(function() {
+      var post_type = $("#post-type").val();
+      if (post_type == "likes") {
+        $("#likes-radio-group").show();
+      } else {
+        $("#likes-radio-group").hide();
+      }
+    });
+  }
 
-	if (page_count == undefined) {
-		page_count = 10;
-	}
+  /**
+   * Sets the offset variable whenever a page number is clicked. The offset is
+   * calculated based on the page number, and is later used in the URL for
+   * the API call
+   * 
+   * num -> int (or string?) - represents the page number clicked
+   * offset -> int - represents which post number to start fetching from
+   */
+  function handlePaginationClick() {
+    $('#page-selection').bootpag({ }).on("page", function(event, num) {
 
-	/**
-	 * Sets the offset variable based on the page number clicked for use
-	 * later in the API call to fetch posts
-	 * 
-	 * num -> int (or string?) representing page number clicked
-	 */
-	$('#page-selection').bootpag({ }).on("page", function(event, num) {
-  	if (num == 1) {
-  		var offset = 0;
-  	}
-  	else {
-  		if ($("#post-type").val() == "likes") {
-  			var offset = (parseInt(num, 10) * 50) - 50;
-  		} else {
-  			var offset = (parseInt(num, 10) * 20) - 20;
-  		}
-  	}
-  	var offset_formatted = "&offset=" + offset.toString();
+      if (num == 1) {
+        var offset = 0;
+      }
+      else {
+        var post_type = $("#post-type").val();
 
-  	// Get the post type the user selected
-  	var post_type = $("#post-type").val();
-  	var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
-    getTumblrPosts(user, post_type, liked_post_type, offset_formatted);
-  });
+        if (post_type == "likes") {
+          // Gets 50 posts at a time
+          var offset = (parseInt(num, 10) * 50) - 50;
+        } else {
+          // Gets 20 posts at a time
+          var offset = (parseInt(num, 10) * 20) - 20;
+        }
+      }
+      var offset_formatted = "&offset=" + offset.toString();
+      var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
+      fetchPosts(user, post_type, liked_post_type, offset_formatted);
+    });
+  }
 
+  /**
+   * Generate div containing post content
+   */
+  function makePost(content) {
+    return '<div class="post">' + content + '</div>';
+  }
+  /**
+   * Generate div containing an error message
+   */
+  function makeError(content) {
+    return '<div id="error"><p class="bg-danger">' + content + '</p></div>';
+  }
 
-	function makePost(content) {
-		return '<div class="post">' + content + '</div>';
-	}
-	function makeError(content) {
-		return '<div id="error"><p class="bg-danger">' + content + '</p></div>';
-	}
+  /**
+   * Properly retrieves post data from the given JSON object, based on the
+   * post type. Calls the makePost function to wrap the data in a div. Returns
+   * a div that is ready to be displayed to the user
+   *
+   * post -> A JSON object representing a Tumblr post
+   * date -> A string representing the date the post was made
+   * type -> A string representing the post type of the given post
+   * liked_post_type -> Optional parameter, if defined we are dealing with liked posts
+   */
+  function formatPost(post, date, type, liked_post_type) {
 
-	function formatPost(post, date, type) {
+    if (type == "photo" || liked_post_type == "photo") {
+      var img_src   = post.photos[0].alt_sizes[1].url;
+      var img_html  = '<img src="' + img_src + '">';
+      var caption   = post.caption;
+      var formatted = makePost(date + img_html + caption);
+    }
 
-		if (type == "photo") {
-			var img_src  = post.photos[0].alt_sizes[1].url;
-			var img_html = '<img src="' + img_src + '">';
-			var caption  = post.caption;
-			var formatted = makePost(date + img_html + caption);
-		}
+    else if (type == "audio" || liked_post_type == "audio") {
+      var player    = post.player;
+      var caption   = post.caption;
+      var formatted = makePost(date + player + caption);
+    }
 
-		else if (type == "audio") {
-			var player  = post.player;
-			var caption = post.caption;
-			var formatted = makePost(date + player + caption);
-		}
+    else if (type == "text" || liked_post_type == "text") {
+      var body = post.body;
+      var formatted = makePost(date + body);
+    }
 
+    return formatted;
+  }
 
-		return formatted;
-	}
+  /**
+   * Check to see if the given API response contains an error
+   */
+  function didError(results) {
+    // Error was returned
+    if (results.meta.status !== 200) {
 
-	function formatLikedPost(post, date, liked_post_type) {
+      // Tumblr username doesn't exist
+      if (results.meta.status === 404) {
+        var error = makeError("ERROR: User not found");
+      }
+      // Tumblr user has restricted his/her likes from being viewed
+      else if (results.meta.status === 401) {
+        var error = makeError("ERROR: User's likes are restricted");
+      }
+      else {
+        var error = makeError("ERROR: Some other error occurred :(");
+      }
+      return [true, error];
+    } else {
+      return [false];
+    }
+  }
 
-		if (liked_post_type == "text") {
-			var body = post.body;
-			var formatted = makePost(date + body);
-		}
+  /**
+   * Connect with the Tumblr API and get posts based on given arguments
+   *
+   * username -> String - The username the user searches for
+   * post_type -> String - Post type the user selected
+   * liked_post_type -> String - The post type for liked posts to fetch
+   * params -> String - Any additional parameters for the API call
+   */
+  function fetchPosts(username, post_type, liked_post_type, params) {
 
-		else if (liked_post_type == "audio") {
-			var player  = post.player;
-			var caption = post.caption;
-			var formatted = makePost(date + player + caption);
-		}
+    if (params === undefined) {
+      params = '';
+    }
 
-		else if (liked_post_type == "photo") {
-			var img_src  = post.photos[0].alt_sizes[1].url;
-			var img_html = '<img src="' + img_src + '">';
-			var caption  = post.caption;
-			var formatted = makePost(date + img_html + caption);
-		}
-		return formatted;
-	}
+    if (post_type == "likes") {
+      var url = "http://api.tumblr.com/v2/blog/" + username + ".tumblr.com/likes?api_key=" + api_key + '&limit=50' + params;
+    }
+    else {
+      var url = "http://api.tumblr.com/v2/blog/" + username + ".tumblr.com" + "/posts/" + post_type + "?api_key=" + api_key + params;
+    }
+    console.log(url);
 
-	function getTumblrPosts(username, type, liked_post_type, params) {
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(results) {
+        console.log(results);
 
-		if (params == undefined) {
-			params = '';
-		}
+        // Dynamically calculate total number of pages to show for the pagination
+        if (post_type != "likes") {
+          var page_count = Math.ceil(results.response.total_posts / 20);
+          console.log("Page Count: " + page_count);
+          console.log("Num Posts: " + results.response.total_posts);
+        } else {
+          var page_count = Math.ceil(results.response.liked_count / 20);
+          console.log("Page Count (Likes): " + page_count);
+          console.log("Num Posts (Likes): " + results.response.liked_count);
+        }
 
-		// API call for liked posts is formatted differently
-		if (type == "likes") {
-			var url = "http://api.tumblr.com/v2/blog/" + username + ".tumblr.com/likes?api_key=" + api_key + '&limit=50' + params;
-		} 
-		// Else, set API call to standard URL
-		else {
-			var url = "http://api.tumblr.com/v2/blog/" + username + ".tumblr.com" + "/posts/" + type + "?api_key=" + api_key + params;
-		}
-		console.log(url);
-		console.log(page_count);
+        // Checks to see if error was returned
+        if (didError(results)[0]) {
+          $("#error").remove();
+          $("#content").append(didError(results)[1]);
 
+          setTimeout(function() {
+            $("#error").fadeOut(400);
+          }, 1000);
+        }
+        
+        // Tumblr username exists
+        else {
 
+          $('#page-selection').bootpag({
+            total: page_count,
+            maxVisible: 10
+          });
 
-		$.ajax({
-			url: url,
-			dataType: 'jsonp',
-			success: function(results) {
-				console.log(results);
+          displayPosts(results);
 
-				// Dynamically calculate total number of pages to show for the pagination
-				if (type != "likes") {
-					page_count = Math.ceil(results.response.total_posts / 20);
-				} else {
-					page_count = Math.ceil(results.response.liked_count / 20);
-				}
+          // All post types default to being stored where key is posts
+          if (post_type != "likes") {
+            var posts = results.response.posts;
+            var num_posts = results.response.posts.length;
+          }
+          // Liked posts are stored where key is liked_posts
+          else {
+            var posts = results.response.liked_posts;
+            var num_posts = results.response.liked_posts.length;
+          }
 
-				// Initialize # of pages/# of pages visible at a time
-				$('#page-selection').bootpag({
-      	  total: page_count,
-      		maxVisible: 10
-	    	});
+          var output = '';
 
+          /**
+           * For each Tumblr post, access the correct fields that correspond with
+           * the post post_type the user requested, in the JSON object returned from the API call
+           */
+          for (var i = 0; i < num_posts; i++) {
+            var date_posted = "<h4>Date Posted: " + posts[i].date + "</h4>";
 
+            // If user wants text posts
+            if (post_type == "text") {
+              output += makePost(date_posted + posts[i].body);
+            }
+            else if (post_type == "photo") {
+              output += formatPost(posts[i], date_posted, post_type);
+            } 
+            else if (post_type == "audio") {
+              // Only display audio posts that don't have the Spotify player
+              if (posts[i].audio_type != "spotify") {
+                output += formatPost(posts[i], date_posted, post_type);
+              }
+            }
+            else if (post_type == "likes") {
+              var current_post_type = posts[i].type;
+              if (current_post_type == liked_post_type) {
+                output += formatPost(posts[i], date_posted, undefined, liked_post_type);
+              }
+            }
+          }
 
-				// Tumblr username doesn't exist
-				if (results.meta.status === 404) {
-					var error = makeError("ERROR: User not found");
-					$("#content").append(error);
-					setTimeout(function() {
-						$("#error").fadeOut(400);
-					}, 1000);
-				}
-				// Tumblr user has restricted his/her likes from being viewed
-				else if (results.meta.status === 401) {
-					var error = makeError("ERROR: User's likes are restricted");
-					$("#content").append(error);
-					setTimeout(function() {
-						$("#error").fadeOut(400);
-					}, 1000);
-				}
-				// Tumblr username exists
-				else {
+          $("#content").empty();
+          $('#content').html(output);
+          console.log(results);
+        }
+      }
+    }); 
+} 
+  /**
+   * Initializes functionality for the search bar when a user enters
+   * a Tumblr username to lookup
+   */
+  function initSearchBar() {
 
-					// All post types default to being stored where key is posts
-					if (type != "likes") {
-						var posts = results.response.posts;
-						var num_posts = results.response.posts.length;
-					}
-					// Liked posts are stored where key is liked_posts
-					else {
-						var posts = results.response.liked_posts;
-						var num_posts = results.response.liked_posts.length;
-					}
+    $("#user-search").on('keydown', function(e) {
+      var post_type = $("#post-type").val();
+      var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
+      // On enter-press
+      if (e.which == 13 || e.keyCode == 13) {
 
-					var output = '';
+        user = $(this).val();
 
-					/**
-					 * For each Tumblr post, access the correct fields that correspond with
-					 * the post type the user requested, in the JSON object returned from the API call
-					 */
-					for (var i = 0; i < num_posts; i++) {
-						var date_posted = "<h4>Date Posted: " + posts[i].date + "</h4>";
-
-						// If user wants text posts
-						if (type == "text") {
-							output += makePost(date_posted + posts[i].body);
-						}
-						else if (type == "photo") {
-							output += formatPost(posts[i], date_posted, type);
-						}	
-						else if (type == "audio") {
-							// Only display audio posts that don't have the Spotify player
-							if (posts[i].audio_type != "spotify") {
-								output += formatPost(posts[i], date_posted, type);
-							}
-						}
-						else if (type == "likes") {
-							var this_post_type = posts[i].type;
-							if (this_post_type == liked_post_type) {
-								output += formatLikedPost(posts[i], date_posted, liked_post_type);
-							}
-						}
-					}
-
-					$("#content").empty();
-					$('#content').html(output);
-					console.log(results);
-				} // end else
-			}
-		});
-}
-
-	$("#user-search").on('keydown', function(e) {
-		var post_type = $("#post-type").val();
-		var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
-		// On enter-press
-		if (e.which == 13 || e.keyCode == 13) {
-			user = $(this).val();
-			if (post_type == "likes" && liked_post_type == undefined) {
-				alert("You must select a post type for the liked posts returned");
-			} else {
-				getTumblrPosts($(this).val(), post_type, liked_post_type, '');
-			}
-			return false;
-		}
-	});
+        if (post_type == "likes" && liked_post_type == undefined) {
+          alert("You must select a post type for the liked posts returned");
+        } else {
+          fetchPosts(user, post_type, liked_post_type, '');
+        }
+        return false;
+      }
+    });
+  }
 
 });
