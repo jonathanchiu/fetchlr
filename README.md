c2dade399971968963579dd6711f75351012713b
Jonathan Chiu <jonathanchiu7@gmail.com>
2014-11-20 21:25:11 -0500

    major additions/changes, check changelog

--- README.md
+++ README.md
@@ -1,22 +1,34 @@
 # Fetchlr
-Fetchlr is a web application that allows a user to "fetch" specific posts from a given Tumblr username/blog and post type. It queries data from the [Tumblr API] and parses variously formatted (format depends on the post type) JSON objects, fetches the posts corresponding to the specified post type, and displays them for the user.
 
-Fetchlr seeks to mitigate the scenarios outlined under "Motivation," and provide specific functionalities that Tumblr lacks, mainly:
+<iframe src="http://gfycat.com/ifr/HonorableClosedDragon" frameborder="0" scrolling="no" width="706" height="752" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>
+
+Fetchlr allows a user to "fetch" posts of a specific post type from a given Tumblr username/blog, and displays them in a clean, minimalistic way. It interacts with the Tumblr API, parses and cleans up the JSON response, and returns a set of posts based on what the user has specified.
+
+Fetchlr aims to provide specific and targetted functionalities which Tumblr currently lacks for regular everyday users, mainly:
   - Getting posts of a specific post type (e.g. text, photo, audio) from a blog
-  - Getting posts of a specific post type from liked posts of a blog
+  - Getting liked posts of a specific post type from a blog
 
-# Motivation
-I am a heavy and daily user of Tumblr and often times, my friends and I want to view specific content on a blog such as text, photo, or audio posts from a specific blog. Unfortunately, most if not all blogs usually post content of <i>all</i> types and it's time consuming and annoying to sift through all of them to find the ones you want. 
+### Motivation/Purpose
+I am a daily user of Tumblr and often times, my friends and I want to view posts of a specific post type, such as photos or audio, from a blog. Sometimes I want to read text posts my friends have posted as well. 
 
-Even more cumbersome is the fact that Tumblr doesn't provide any way for users to filter posts you, or other people, have liked. The "liked" page of a Tumblr user contains <i>all</i> posts the user has liked since the creation of their account, potentially thousands of posts (I currently have about 3,000 liked posts).
+Most if not all blogs:
+  - Make posts of varying types (text, audio, photo, quotes, etc)
+  - Have potentially made thousands of posts since the creation of their account
 
-In both scenarios, and with the way Tumblr is currently setup, one can't narrow down posts to only one post type. If I wanted to view only audio posts, I'd potentially have to sift through hundreds of other posts before I find an audio post.
+It can be quite time consuming to sift through thousands of posts before coming across the type of post I want to see. With how Tumblr is setup currently, if I want to only view the photo posts a blog has posted, I can't. Tumblr provides an archive link for all blogs, which "archives" all posts ever made, but it doesn't filter on post type, and only filters on month and year (whose result set could again, contain potentially hundreds of posts, all of different post types).
+
+Additionally Tumblr doesn't provide any way for users to view liked posts of a specific type. The "liked" page of a Tumblr user contains <i>all</i> posts the user has liked since the creation of their account, potentially containing thousands of posts (I myself have about 5,000 liked posts). A similar scenario arises: I like posts of all types, but if I want to view only the photo posts I've liked, I can't. I have to sift through potentially hundreds of other posts of varying types before I come across a photo.
+
+Fetchlr seeks to mitigate the above scenarios!
 
 ### Tech
 Fetchlr utilizes the following technologies and wouldn't have been possible without them:
 * [Twitter Bootstrap]
 * [jQuery]
-* [jQuery bootpag] - Helped greatly with pagination
+* [jQuery bootpag] - Neat jQuery plugin that helped greatly pagination
+* [Digital Ocean] - Hosted on a Digital Ocean droplet
+* [node.js] - Powered by Node.js (web server file not in the repo)
+* [tumblrwks] - Node.js package for easier and syntactically cleaner interaction with Tumblr API
 
 ### Todo's
 Fetchlr still needs a lot of work. If you have suggestions or found a bug, please shoot me an email!
@@ -24,17 +36,33 @@ Fetchlr still needs a lot of work. If you have suggestions or found a bug, pleas
  - Clean-up (naming, whitespace, etc) and abstract more of the Javascript code
  - Improve generation of dynamic pagination
  - Improve general application design, and styling of displayed posts (the CSS needs a lot of work)
+ - Work on logic for filtering likes of a specific post type
+ - General bug fixes
 
- ### Changelog
- 11/15/14
+### Changelog
+11/15/14
  - Added ability to fetch video posts
  - Added infinite scroll option so users don't have to access pagination to get next set of posts
  - Fixed up some pagination logic, still needs a little work
  - Cleaned-up JS code, still needs work
+
+11/18/14
+ - Delegated fetching of post data to server, also obscures API keys from public eyes! (seems to have made fetching of data a lot quicker as well, but I'm not 100% sure..)
+ - Make any images or videos returned responsive
+ - Changed font to Montserrat
+ - Removed filtering on likes for the time being, until I can find a better way to accurately display the result set
+ - Added ability to fetch quote posts
+ - Improved error handling
+ - Added corresponding post tags to all posts displayed, which link back to user's Tumblr e.g. bob.tumblr.com/tagged/tag_name_here
+ - Updated README.md
+
+11/20/14
+ - All post types have now been accounted for! :)
 
 [Tumblr API]:https://www.tumblr.com/docs/en/api/v2
 [Digital Ocean]:https://www.digitalocean.com
 [node.js]:http://nodejs.org
 [Twitter Bootstrap]:http://twitter.github.com/bootstrap/
 [jQuery]:http://jquery.com
+[tumblrwks]:https://github.com/arkxu/tumblrwks
 [jQuery bootpag]:http://botmonster.com/jquery-bootpag/
--- main.js
+++ main.js
@@ -1,27 +1,10 @@
 $(function() {
 
-	var api_key        = "";
 	var posts_per_page = 10;
   var user;
 
   initSearchBar();
-  handleLikesSelect();
   handlePaginationClick();
-
-  /**
-   * Handles showing/hiding of additional filter options when
-   * user selects the "Likes" post type
-   */
-  function handleLikesSelect() {
-    $("#post-type").change(function() {
-      var post_type = $("#post-type").val();
-      if (post_type == "likes") {
-        $("#likes-radio-group").show();
-      } else {
-        $("#likes-radio-group").hide();
-      }
-    });
-  }
 
   /**
    * Sets the offset variable whenever a page number is clicked. The offset is
@@ -34,21 +17,16 @@ $(function() {
   function handlePaginationClick() {
     $('#page-selection').bootpag({ }).on("page", function(event, num) {
 
+      var offset;
+      var post_type = $("#post-type").val();
+
       if (num == 1) {
-        var offset = 0;
+        offset = 0;
       }
       else {
-        var post_type = $("#post-type").val();
-
-        if (post_type == "likes") {
-          var offset = (parseInt(num, 10) * 50) - 50;
-        } else {
-          var offset = (parseInt(num, 10) * 10) - 10;
-        }
+        offset = (parseInt(num, 10) * 10) - 10;
       }
-      var params = "&offset=" + offset + "&limit=" + posts_per_page;
-      var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
-      fetchPosts(user, post_type, liked_post_type, params);
+      fetchPosts(user, post_type, offset, posts_per_page);
     });
   }
 
@@ -62,7 +40,12 @@ $(function() {
    * Generate div containing an error message
    */
   function makeError(content) {
-    return '<div id="error"><p class="bg-danger">' + content + '</p></div>';
+    var error = '<div id="error"><p class="bg-danger">' + content + '</p></div>';
+    $("#content").append(error);
+
+    setTimeout(function() {
+      $("#error").fadeOut(400);
+    }, 1000);
   }
 
   /**
@@ -73,32 +56,72 @@ $(function() {
    * post -> A JSON object representing a Tumblr post
    * date -> A string representing the date the post was made
    * type -> A string representing the post type of the given post
-   * liked_post_type -> Optional parameter, if defined we are dealing with liked posts
    */
-  function formatPost(post, date, type, liked_post_type) {
+  function formatPost(post, date, type) {
 
   	var formatted;
+    var liked_post_type = post.type;
+    var tags = '';
+
+    if (post.tags.length > 0) {
+      $.each(post.tags, function(i, v) {
+        var tag = "#" + v + " ";
+        var url = "http://" + user + ".tumblr.com/tagged/" + v.replace(/ /g, "-");
+        tags += '<a href="' + url + '" target="_blank">' + tag + '</a>'
+      });
+    }
+
+    var all_tags = '<div class="tags">' + tags + '</div>';
 
     if (type == "photo" || liked_post_type == "photo") {
-      var img_src   = post.photos[0].alt_sizes[1].url;
-      var img_html  = '<img src="' + img_src + '">';
-      var caption   = post.caption;
-      formatted = makePost(date + img_html + caption);
+			var img_src  = post.photos[0].alt_sizes[1].url;
+			var img_html = '<img class="photo" src="' + img_src + '">';
+			var caption  = post.caption;
+			formatted    = makePost(date + img_html + caption + all_tags);
     }
 
     else if (type == "audio" || liked_post_type == "audio") {
-      var player    = post.player;
-      var caption   = post.caption;
-      formatted = makePost(date + player + caption);
+			var player  = post.player;
+			var caption = post.caption;
+			formatted   = makePost(date + player + caption + all_tags);
     }
 
     else if (type == "text" || liked_post_type == "text") {
 			var body  = post.body;
-			formatted = makePost(date + body);
-    } 
+			formatted = makePost(date + body + all_tags);
+    }
 
+    else if (type == "quote" || liked_post_type == "quote") {
+      var quote = '<div class="quote"><blockquote>' + post.text + '</blockquote></div>';
+      var quote_source = '<div class="quote-source">' + post.source + '</div>';
+      formatted = makePost(date + quote + quote_source + all_tags);
+    }
+
+    else if (type == "answer" || liked_post_type == "answer") {
+      var question = '<div class="question"><blockquote>' + post.question + '</blockquote></div>';
+      var answer = '<div class="answer">' + post.answer + '</div>';
+      formatted = makePost(date + question + answer + all_tags);
+    }
+
+    else if (type == "link" || liked_post_type == "link") {
+      var title = "<h3>" + post.title + "</h3>";
+      var description = post.description;
+      var original_url = '<a target="_blank" href="' + post.url + '">Source</a>'
+      formatted = makePost(date + title + description + original_url + all_tags);
+    }
+
+    else if (type == "chat" || liked_post_type == "chat") {
+      var chat = '<div class="chat">';
+      var body = post.body.split("\r\n");
+      var chat_length = body.length;
+      for (var i = 0; i < chat_length; i++) {
+        chat += body[i] + '<br>';
+      }
+      chat += "</div>"
+      formatted = makePost(date + chat + all_tags)
+    }
+// magic of love
     else if (type == "video") {
-
     	/**
     	 * Given HTML string representing video embed, convert it to a jQuery
     	 * object, set its attributes to make it responsive, and return the
@@ -125,7 +148,7 @@ $(function() {
     		var embed_video = post.player[2].embed_code;
     	}
 
-    	formatted = makePost(date + local(embed_video) + post.caption);
+    	formatted = makePost(date + responsify(embed_video) + post.caption + all_tags);
     }
 
     return formatted;
@@ -142,6 +165,8 @@ $(function() {
 
     // Error was returned
     if (results.meta.status !== 200) {
+
+    	$("#page-selection").hide();
 
       // Tumblr username doesn't exist
       if (results.meta.status === 404) {
@@ -165,90 +190,120 @@ $(function() {
    *
    * username -> String - The username the user searches for
    * post_type -> String - Post type the user selected
-   * liked_post_type -> String - The post type for liked posts to fetch
    * params -> String - Any additional parameters for the API call
    */
-  function fetchPosts(username, post_type, liked_post_type, params) {
+  function fetchPosts(username, post_type, offset, limit) {
 
-    if (params === undefined) {
-      params = '';
-    }
+    //var tag       = $("#tag").val();
+    var post      = (post_type == "likes") ? "likes" : "posts";
+    var blog_name = username + ".tumblr.com";
+    var url       = "/fetch";
 
-    if (post_type == "likes") {
-      var url = "http://api.tumblr.com/v2/blog/" + 
-      					username + ".tumblr.com/likes" + 
-      					"?api_key=" + api_key + 
-      					'&limit=50' + params;
-    }
-    else {
-      var url = "http://api.tumblr.com/v2/blog/" + 
-      					username + ".tumblr.com" + 
-      					"/posts/" + post_type + 
-      					"?api_key=" + api_key + 
-      					params;
-    }
-    
-    console.log(url);
+    var data = JSON.stringify({
+      blog_name: username,
+      post: post,
+      post_type: post_type,
+      offset: offset,
+      limit: limit
+      //tag: tag
+    });
 
+    /**
+     * Get request to Node.js to fetch data
+     */
     $.ajax({
       url: url,
-      dataType: 'jsonp',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: data,
+      beforeSend: function() {
+        var loader = '<img id="loader" style="display:block; margin:0 auto" src="loader.gif">';
+        $("#content").append(loader);
+      },
       success: function(results) {
-
-      	var content = $("#content");
-
         console.log(results);
+        var content = $("#content");
+        $("#loader").remove();
 
-        // Dynamically calculate total number of pages to show for the pagination
-        if (post_type != "likes") {
-          var page_count = Math.ceil(results.response.total_posts / 10);
-          console.log("Page Count: " + page_count);
-          console.log("Num Posts: " + results.response.total_posts);
-        } else {
-          var page_count = Math.ceil(results.response.liked_count / 10);
-          console.log("Page Count (Likes): " + page_count);
-          console.log("Num Posts (Likes): " + results.response.liked_count);
+        var error = jQuery.isEmptyObject(results);
+        var no_posts = (post_type == "likes") ? 
+                        (results.liked_posts.length == 0) : 
+                        (results.posts.length == 0);
+
+        if (error) {
+          $("#page-selection").hide();
+          if (error && post_type == "likes") {
+            makeError("That user's likes are restricted from being viewed publicly.");
+          } else {
+            makeError("The username you entered does not exist");
+          }
         }
 
-        // Check to see if API response contained error
-        if (didError(results)[0]) {
-
-        	// Remove any previously generated error msg from DOM
-          $("#error").remove();
-          content.append(didError(results)[1]);
-
-          setTimeout(function() {
-            $("#error").fadeOut(400);
-          }, 1000);
+        else if (no_posts) {
+          makeError("No posts were found.");
         }
-        
-        // Tumblr username exists
-        else {
+
+        // else if (no_posts) {
+        //   makeError("No " + post_type + " posts with tag " + '"' + tag + '"' + " were found.");
+        // }
+
+        else { 
+          $("#page-selection").show();
+          var output  = '';
+
+          // if (tag == '') {
+          //   $("#pagination-preference").val("manual");
+          // } else {
+          //   $("#pagination-preference").val("infinite");
+          //   $("#page-selection").hide();
+          // }
+
+          // Dynamically calculate total number of pages to show for the pagination
+          if (post_type != "likes") {
+            var page_count = Math.ceil(results.total_posts / 10);
+            console.log("Page Count: " + page_count);
+            console.log("Num Posts: " + results.total_posts);
+          } else {
+            var page_count = Math.ceil(results.liked_count / 10);
+            console.log("Page Count (Likes): " + page_count);
+            console.log("Num Posts (Likes): " + results.liked_count);
+          }
 
           $('#page-selection').bootpag({
             total: page_count,
-            maxVisible: 10
+            maxVisible: 5,
+            next: 'Next',
+            prev: 'Previous',
+            leaps: false
           });
 
           // All post types default to being stored where key is posts
           if (post_type != "likes") {
-            var posts = results.response.posts;
-            var num_posts = results.response.posts.length;
+            var posts = results.posts;
+            var num_posts = results.posts.length;
           }
           // Liked posts are stored where key is liked_posts
           else {
-            var posts = results.response.liked_posts;
-            var num_posts = results.response.liked_posts.length;
+            var posts = results.liked_posts;
+            var num_posts = results.liked_posts.length;
           }
-
-          var output = '';
 
           /**
            * For each Tumblr post, access the correct fields that correspond with
            * the post post_type the user requested, in the JSON object returned from the API call
            */
           for (var i = 0; i < num_posts; i++) {
-            var date_posted = "<h4>Date Posted: " + posts[i].date + "</h4>";
+            var date       = new Date(posts[i].date);
+            var time       = date.toLocaleTimeString();
+            var month      = date.getUTCMonth() + 1;
+            var day        = date.getUTCDate();
+            var year       = date.getUTCFullYear();
+            var time_stamp = month + "/" + day + "/" + year;
+
+            var date_posted = '<div class="date">' + "<h4>" + time_stamp + "</h4>" +
+                              '<h4 style="padding-bottom:0.5em;color:#999">' + time + "</h4></div>";
+
 
             if (post_type == "audio") {
               // Only display audio posts that don't have the Spotify player
@@ -256,13 +311,8 @@ $(function() {
                 output += formatPost(posts[i], date_posted, post_type);
               }
             }
-            else if (post_type == "likes") {
-              var current_post_type = posts[i].type;
-              if (current_post_type == liked_post_type) {
-                output += formatPost(posts[i], date_posted, undefined, liked_post_type);
-              }
-            } else {
-            	output += formatPost(posts[i], date_posted, post_type);
+            else {
+              output += formatPost(posts[i], date_posted, post_type);
             }
           }
 
@@ -273,16 +323,17 @@ $(function() {
            * If manual, always set to new posts, getting rid of all previous posts
            */
           if (pagination_preference == "infinite") {
-          	content.append(output);
+            console.log("infinite");
+            content.append(output);
           } else {
-          	content.html(output);
+            content.html(output);
           }
 
           // For each image returned in a post, add the img-responsive class
           content.find('img').each(function() {
-          	$(this).attr('class', 'img-responsive');
+            $(this).addClass('img-responsive');
           });
-          
+
           /**
            * 
            * Let's us know if we are good to go to load more data, prevents
@@ -291,27 +342,26 @@ $(function() {
           var content_loaded = 1;
 
           if (pagination_preference == "infinite") {
-          	$("#page-selection").hide();
 
-	          // Load next set of posts once user has scroll passed a certain point
-	          $(document).scroll(function() {
-					  	var units_from_bottom = $(document).height() - $(window).height() - $(window).scrollTop();
+            $("#page-selection").hide();
 
-					  	if (units_from_bottom < 500 && content_loaded == 1) {
-					  		$("#page-selection a:last").click();
-					  		content_loaded = 0;
-					  	}
-					  });
+            // Load next set of posts once user has scroll passed a certain point
+            $(document).scroll(function() {
+              var units_from_bottom = $(document).height() - $(window).height() - $(window).scrollTop();
 
-	        } else {
-	        	$("#page-selection").show();
-	        }
-
-          console.log(results);
+              if (units_from_bottom < 500 && content_loaded == 1) {
+                // Get the next page of posts
+                $("#page-selection a:last").click();
+                content_loaded = 0;
+              }
+            });
+          }
         }
-      }
-    }); 
-} 
+      } // End success function
+    });
+    console.log(url);
+  }
+
   /**
    * Initializes functionality for the search bar when a user enters
    * a Tumblr username to lookup
@@ -319,22 +369,21 @@ $(function() {
   function initSearchBar() {
 
     $("#user-search").on('keydown', function(e) {
+
       var post_type = $("#post-type").val();
-      var liked_post_type = $("input:radio[name=likes-post-type]:checked").val();
       // On enter-press
       if (e.which == 13 || e.keyCode == 13) {
+
+      	$("#pagination-preference").hide();
 
       	$("#content").empty();
       	$("#page-selection").bootpag({
       		page: 1
       	});
+
         user = $(this).val();
 
-        if (post_type == "likes" && liked_post_type == undefined) {
-          alert("You must select a post type for the liked posts returned");
-        } else {
-          fetchPosts(user, post_type, liked_post_type, '&limit=' + posts_per_page);
-        }
+        fetchPosts(user, post_type, 0, posts_per_page);
         return false;
       }
     });
